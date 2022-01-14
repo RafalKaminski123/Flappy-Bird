8cb63fb185716ef6ef50953117174e3656bd1b2e
Rafał Kamiński <kaminskirafal33@gmail.com>
2022-01-14 20:29:46 +0100

    mechanics creation

--- FlappyBird/Assets/scripts/PipiesController.cs
+++ FlappyBird/Assets/scripts/PipiesController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PipiesController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject prefab;
+    public float spawnRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+    private void OnEnable()
     {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+    private void Spawn()
+    {
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);    
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
